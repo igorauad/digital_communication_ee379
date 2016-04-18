ff33662714542f2a325636aa7fef8fa40a1dfe4d
Igor Freire <igor.auad@gmail.com>
2016-04-18 11:33:19 -0300

    Add example 4.3.1 about water filling

---
+++ chap4/example_4_3_1-water_filling.m
@@ -0,0 +1,118 @@
+%% Example 4.3.1 - Water filling
+
+% Parameters
+N       = 8;    % FFT size and the number of dimensions
+gap_db  = 0;    % SNR gap to capacity (dB)
+Ex      = N;    % Total energy budget for each OFDM symbol
+
+
+% Derivations
+N_bar   = N/2 + 1;        % Used subchannels (including DC and Nyquist)
+Ex_bar  = Ex / N;         % Energy per dimension
+gap     = 10^(gap_db/10); % Gap in linear scale
+
+% Noise energy per dimension (N0/2):
+sigma_n = 0.181;
+
+%% Channel
+
+h = [0.9 1];
+
+H = fft(h, N);
+
+figure
+plot(10*log10(abs(H).^2))
+xlabel('Subcarrier (n)')
+title('Channel Response')
+grid on
+
+%% SNRn when the transmitter applies unit energy to the subchannel
+
+gn = abs(H).^2 / sigma_n;
+
+%% Compute bit-loading using waterfilling
+% This distributes the energy budget Ex over the subchannels with the
+% objective of maximizing the data-rate. As a result, a non-integer number
+% of bits is obtained as well as the corresponding energy allocated to each
+% dimension of the subchannels.
+
+[bn_bar, En_bar] = waterFilling(gn, Ex_bar, N, gap)
+
+% Sanity check. Is the sum of the energy in each dimension less than the
+% total?
+fprintf('Total allocated energy: \t %g \n', sum(En_bar));
+
+% Ideally they should be equal, because this is the constraint in the
+% optimization.
+
+% Total number of bits per dimension
+b_bar = sum(bn_bar) / N;
+
+figure
+stem(bn_bar(1:N_bar), 'linewidth', 1.2)
+xlabel('Subchannel n')
+ylabel('$b_n$ (dB)')
+grid on
+
+%% SNR per subchannel
+
+SNRn = En_bar .* gn;
+
+figure
+stem(10*log10(SNRn))
+xlabel('Subchannel n')
+ylabel('$SNR_n$ (dB)')
+grid on
+
+%% Resulting multi-channel SNR
+% "a single SNR measure that characterizes the set of subchannels by an
+% equivalent single AWGN that achieves the same data rate"
+
+SNRm_u = ((prod(1 + SNRn/gap)^(1/N)) - 1) * gap
+
+% Alternative way of computing the number of bits per dimension:
+b_bar_2 = 0.5 * log2(1 + SNRm_u/gap)
+
+%% Margin
+
+% First, note the difference between the gap and the margin definitions:
+%
+%   The gap is obtained by noting that any spectral efficiency should be
+%   lower than the channel capacity, i.e. rho < C = log2(1+SNR). The
+%   inequality can be stated as:
+%
+%       SNR > 2^rho - 1
+%
+%   Which leads to the conclusion that "2^rho - 1" is a lower bound on the
+%   SNR.
+
+% Channel Capacity
+C  = log2(1 + SNRm_u); % Bits per 2 dimensions (b/2D)
+% The acual spectral efficiency must be lower than C:
+spec_effiency =  2*b_bar; % Bits per 2 dimensions (b/2D)
+% SNR lower bound:
+SNR_minimum = 2^spec_effiency - 1;
+% Gap to capacity:
+gap_computed = SNRm_u / SNR_minimum;
+% See Chap. 4 of Forney's course on Principles of Digital Communications II
+% at MIT OCW
+
+% However, sometimes the spectral effiency is arbitrarily reduced such that
+% more margin is left for the errors in the communication. By recalling
+% that spectral efficiency is R / W, one concludes normally this implies a
+% lower data rate. 
+%   The constrained spectral effiency is given by:
+%       2 * b_bar_constrained = log2(1 + SNR/(gap * gamma))
+%   Hence, gamma (the margin) can be obtained by:
+%       gamma = (SNR/gap) / (2^(2*b_bar_constrained) - 1)
+%   Finally, note (SNR/gap) is the SNR lower bound of Shannon, which is
+%   equivalent to 2^(2*b_bar_unconstrained) - 1.
+
+% Definition in (4.7):
+gamma_m = (SNRm_u/gap) / (2^(2*b_bar) - 1);
+
+% In dB:
+gamma_m_db = 10*log10(gamma_m)
+
+% Note: margin is expected to be very low, since we are maximizing
+% data-rate at its expense.
---
+++ lib/waterFilling.m
@@ -0,0 +1,87 @@
+function [ bn_bar, En_bar, iUsed ] = waterFilling(gn, Ex_bar, N, gap)
+%WATERFILLING Water-filling bit-loading solution
+%   Finds the rational bit-load that satisfies the water-filling solution
+%   subject to the constraint that Ex = Ex_bar*N, namely the transmit
+%   energy is equal to the Energy per dimension * number of dimensions.
+%
+%   Inputs
+% gn      -> SNRn when the transmitter applies unit energy per dimension
+%            of the subchannel.
+% Ex_bar  -> Total energy per dimension
+% N       -> Number of dimensions
+% gap     -> Linear SNR gap to capacity
+%
+%   Outputs
+% bn_bar  -> Optimal bit loading vector per dimension
+% En_bar  -> Energy per dimension
+% iUsed   -> Used dimensions
+%
+%
+% Note: two dimensions can pertain to the same subchannel. For example, for
+% DMT, the vector always has 2 real subchannels at DC and Nyquist and N/2 -
+% 1 complex subchannels with two real dimensions.
+
+% Total energy budget
+Ex = Ex_bar * N;
+
+%% Initialization
+
+% Preallocate output vectors:
+En_bar  = zeros(1, N);
+bn_bar  = zeros(1, N);
+
+% Initialize the vector of indexes corresponding to the used dimensions:
+iUsed = find(gn ~= 0);
+
+%% Run search for the water-fill level:
+
+i = 0;
+positiveEnergies = 0;
+
+while (~positiveEnergies)
+    gn_vector = gn(iUsed);
+    
+    N_i = N - i;
+    
+    % Use (4.44) to compute the constant that satisfy the water-filling 
+    % equation (4.24):
+    K = (1 / N_i) * (Ex +  gap * sum( 1 ./ gn_vector ) );
+    
+    % Then, the corresponding energies for each dimension (Eq. 4.45):
+    En_bar_i = K - gap*(1./gn_vector);
+    
+    % Find the one with the minimum energy
+    [minEn, iMinEn] = min(En_bar_i);
+    
+    % Is the minimum energy negative?
+    if (minEn < 0)
+        % Remove the dimension if minimum energy from the vector of usable
+        % dimensions:
+        iUsed(iMinEn) = [];
+        % Attempt one more time
+        i = i + 1;
+    else
+        % All energies are positive -> constant K was found!
+        positiveEnergies = 1;
+        
+        fprintf('Water-fill level: %g \n', K);
+    end
+    
+end
+
+%% Optimal loading:
+
+% Energies per per dimension (in this vector, a 0 means the dimension
+% should not be used):
+En_bar(iUsed) = En_bar_i;
+
+% Aslanis formula (4.46) at the used dimensions:
+b_bar_used = .5 * log2(K * gn(iUsed) / gap);
+
+% Fill the complete output vector (where 0 means the dimension is not 
+% used):
+bn_bar(iUsed) = b_bar_used;
+
+
+end
+
