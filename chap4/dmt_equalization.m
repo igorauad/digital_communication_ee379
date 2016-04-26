5cc13e9c83b2069ed97de3b321d0c3d8b7838414
Igor Freire <igor.auad@gmail.com>
2016-04-26 09:46:15 -0300

    Add computation of constellation minimum distances

--- chap4/dmt_equalization.m
+++ chap4/dmt_equalization.m
@@ -362,13 +362,23 @@ for k = 1:(N/2 + 1)
     end
 end
 
-%% Energy loading (constellation scaling factors)
+%% Energy loading (constellation scaling factors) and minimum distances
 % Note 2-dimensional subchannels whose bit loading is 1 (i.e. M=2) use a
 % QAM constellation equivalent to a 2-PAM rotated by 90 degrees, which
 % implies the two dimensions are really used regardless.
+%
+% For the computation of d_min for each subchannel, note it is equivalent
+% to 2*Scale_n(k) for QAM-SQ and sqrt(2)*2*Scale_n(k) for QAM-Hybrid.
+%
+% Finally, note scaling factors and corresponding minimum distances are
+% computed only for the positive half of the spectrum and notice the fact
+% that the Hermitian symmetry will double the energy in a certain
+% subchannel  is already pre-compensated by allocating only half of the
+% subchannel energy budget through the scaling factor.
 
 % Preallocate
-Scale_n = zeros(N/2 + 1, nSymbols); % Constellation scaling factors
+Scale_n = zeros(N/2 + 1, 1); % Constellation scaling factors
+dmin_n  = zeros(N/2 + 1, 1); % Constellation minimum distances
 
 % Scale factors for each subchannel n to achieve the En average energy
 for k = 1:(N/2 + 1)
@@ -397,6 +407,34 @@ for k = 1:(N/2 + 1)
                     modulator{modem_n(k)}.constellation,...
                     'avpow', En_discrete(k));
             end
+        end
+    end
+end
+
+% Minimum distances
+for k = 1:(N/2 + 1)
+    if (En_bar(k) > 0 && modOrder(k) > 1)
+        if (dim_per_subchannel(k) == 2)
+            % Get the k-th subchannel bit load, depending on the loading
+            % strategy:
+            if (loading == 0)
+                b_k = bn(k);
+            else
+                b_k = bn_discrete(k);
+            end
+
+            %% Compute the minimum distance
+            if (mod(floor(b_k),2) ~= 0 && ...
+                    floor(b_k) ~= 1)
+                % For Hybrid QAM
+                dmin_n(k) = sqrt(2)*2*Scale_n(k);
+            else
+                % For SQ-QAM, including the "rotated" 2-PAM used
+                % for two-dimensional subchannels loaded with bk=1
+                dmin_n(k) = 2*Scale_n(k);
+            end
+        else
+            dmin_n(k) = 2*Scale_n(k);
         end
     end
 end
