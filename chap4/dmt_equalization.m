10db9f41a6eb7d92cbbfa54acd0a69c78c26bfa9
Igor Freire <igor.auad@gmail.com>
2016-04-27 09:59:27 -0300

    Add Shortening SNR evaluation

--- chap4/dmt_equalization.m
+++ chap4/dmt_equalization.m
@@ -122,11 +122,21 @@ fprintf('\n-------------------- MMSE-TEQ Design ------------------- \n\n');
         % New Unitary-energy SNR:
         gn_teq = (abs(H_teq).^2) / unbiased_error_energy_per_dim;
 
-
         % When the TEQ is employed, the effective pulse response
         % becomes:
         p_eff = conv(p,w);
-
+        % Compute the "Shortening SNR" (SSNR), which measures the ratio
+        % between the energy within the window of samples where MMSE should
+        % concentrate the energy and outside the window.
+        p_eff_indexes = 1:length(p_eff);
+        % nu + 1 consecutive samples where energy should be concentrated
+        win_indexes = delta+1:delta+nu+1;
+        p_win = p_eff(win_indexes);
+        % remaining samples
+        wall_indexes = setdiff(p_eff_indexes, win_indexes);
+        p_wall = p_eff(wall_indexes);
+        ssnr = (p_win * p_win') / (p_wall * p_wall');
+        fprintf('SSNR:\t %g dB\n', 10*log10(ssnr));
     otherwise
         % When MMSE-TEQ is not used, at least a bias should be generically
         % defined:
