7aec72d8a4fcec3e471bac7241961f3ce2c50041
Igor Freire <igor.auad@gmail.com>
2016-07-14 11:40:01 -0300

    Update TEQ optimization function considering the new TEQ designs

--- lib/optimizeTeq.m
+++ lib/optimizeTeq.m
@@ -1,77 +1,148 @@
-function [ nTaps_o, delta_o ] = optimizeTeq(criterion, p, nu, sigma, Ex_bar, gap, N, Lmax, delta_min, delta_max)
-%optimizeTeq Searches the optimal TEQ length and delay
+function [ nTaps_o, delta_o ] = optimizeTeq(type, p, nu, l, sigma, Ex_bar, N, debug)
+%optimizeTeq Searches TEQ length and delay parameters
 %   TEQ performance is very sensitive to the chosen delay and TEQ length.
 %   An analytical formulation for these parameters is not available, so the
-%   approach is to perform a search for the optimal parameters. According
-%   to the dissertation in [1], there are three main search criteria: the
-%   MSE, the Shortening SNR (SSNR) and the Geometric SNR. The three modes
-%   of operation are configurable in this script
+%   approach is to perform a search for the optimal parameters. As
+%   summarized in the dissertation [1], there are three main TEQ designs:
+%   based on MMSE, the Shortening SNR (SSNR) and the Geometric SNR. The
+%   three modes of operation are configurable in this script.
+%
+%   General (but loose) guidelines for the choices of delay and length are
+%   presented in [3]. Nonetheless, specific conditions are applicable to
+%   each type of TEQ design.
 %
 % Inputs:
-%   criterion   =>    Optimization criterion
-%           0) Optimize using MSE;
-%           1) Optimize using SSNR;
-%           2) Optimize using Geo-SNR;
-%   p           =>    channel pulse response
-%   nu          =>    the dispersion of the desirable pulse response (b)
-%   sigma       =>    noise power in linear scale
-%   Ex_bar      =>    Tx Energy per dimension
-%   Lmax        =>    Maximum number of taps
+%   type    =>  TEQ design criterion
+%                   0) Minimization of MSE;
+%                   1) Maximimization of Shortening SNR (SSNR);
+%                   2) Maximimization of Geometric SNR (GeoSNR);
+%   p       =>  Channel pulse response
+%   l       ->  Oversampling factor
+%   nu      =>  Memory of the target impulse response (b)
+%   sigma   =>  Noise energy per dimension
+%   Ex_bar  =>  Signal energy per dimension
 %
 %
 %   References:
 % [1] http://users.ece.utexas.edu/~bevans/projects/adsl/index.html
 % [2] N. Al-Dhahir and J. Cioffi, "Optimum finite-length equalization for
-% multicarrier transceivers," Global Telecommunications Conference, 1994.
-% GLOBECOM '94. Communications: The Global Bridge., IEEE, San Francisco,
-% CA, 1994, pp. 1884-1888 vol.3.
-%
-if (delta_min > delta_max)
-    error('Search range is null');
+%     multicarrier transceivers," Global Telecommunications Conference,
+%     1994. GLOBECOM '94. Communications: The Global Bridge., IEEE,
+%     San Francisco, CA, 1994, pp. 1884-1888 vol.3.
+% [3] R. K. Martin and C. R. Johnson, "Adaptive equalization: transitioning
+%     from single-carrier to multicarrier systems," in IEEE Signal
+%     Processing Magazine, vol. 22, no. 6, pp. 108-122, Nov. 2005.
+% [4] P. J. W. Melsa, R. C. Younce and C. E. Rohrs, "Impulse response
+%     shortening for discrete multitone transceivers," in IEEE Transactions
+%     on Communications, vol. 44, no. 12, pp. 1662-1672, Dec 1996.
+
+if (nargin > 7)
+    if (debug)
+        show_plots = 1;
+    else
+        show_plots = 0;
+    end
+else
+    show_plots = 0;
 end
-% Initialize
-L_vec     = 1:Lmax; % Vector of TEQ lengths
-delta_vec = delta_min:delta_max; % Vector of delays
+
+%% Equalizer Length Range
+% Equalizer length is advised in [3] to be 3-5 times the difference of
+% channel and prefix lengths. In contrast, [4] states that typical DMT
+% transceivers use an equalizer (there called SIRF) length which is shorter
+% than the length of the CP. We adopt the guidelines of [3] in the sequel.
+% However, in the specific case of MaxSSNR TEQ, the equalizer length must
+% be less than or equal to the cyclic prefix length in order for one matrix
+% of the derivation to be positive semidefinite. Therefore, we also
+% consider the exceptional case of [4].
+
+switch (type)
+    case {0,2} % MSE and GeoSNR
+        maxTaps  = 5*(length(p) - nu);
+        minTaps  = 3*(length(p) - nu);
+    case 1 % SSNR
+        maxTaps  = nu;
+        minTaps  = l;
+    otherwise
+        error('Non-available TEQ type');
+end
+
+% Avoid null number of symbol-spaced taps (floor of a div by l is used):
+if (minTaps < l)
+    minTaps = l;
+end
+
+% Vector of TEQ lengths:
+nTap_vec = minTaps:maxTaps;
+
+%% Delay Range
+% Desired delay is advised in [3] to be the location of nu-length window of
+% channel with largest energy + half the length of the shortener.
+
+win_energy = zeros(length(p) - nu, 1);
+for i = 1:length(p) - nu
+    win_energy(i) = norm(p(1+i : i+nu))^2;
+end
+% Maximum windowed energy
+[~, iMax]  = max(win_energy);
+% Location of the window center
+iWinCenter = iMax + nu/2;
+
+% Minimum delay in the range
+delta_min  = iWinCenter - ceil(maxTaps/2);
+if (delta_min < 0)
+    delta_min = 0;
+end
+
+% Maximum delay in the range
+delta_max  = iWinCenter + ceil(maxTaps/2);
+
+% Vector of delays:
+delta_vec  = delta_min:delta_max;
+
+%% Initialize
 
 % Constants
-filtertype    = 1; % FIR Filter Type
-nTap_length   = length(L_vec);
+nTap_length   = length(nTap_vec);
 nDelta_length = length(delta_vec);
 
 % Preallocate
 objective = NaN * ones(nTap_length, nDelta_length);
 
-gap_db = 10*log10(gap);
-
 %% Search
 for i = 1:nTap_length
     % Number of taps
-    L = L_vec(i);
+    nTaps = nTap_vec(i);
+
     % The delay has to be less than or equal to the number of taps
-    iDeltaLength = sum(delta_vec <= L);
+    iDeltaLength = sum(delta_vec <= nTaps);
 
     % Search delay
     for k = 1:iDeltaLength
         % Delay
         delta = delta_vec(k);
 
-        % Design the TEQ and evaluate the resulting SNRmfb:
-        [w, b, SNRteq, ~] = ...
-            teq(p, L, nu, delta, sigma, Ex_bar, filtertype);
-
-        switch (criterion)
+        switch (type)
             case 0 % MSE
-                criterion_label = 'MSE';
+                type_label = 'MSE';
+                objective_label = '10\\log(SNRmfb)';
+                % Design the TEQ and evaluate the resulting SNRmfb:
+                [w, SNRteq] = ...
+                    mmse_teq(p, l, delta, floor(nTaps/l), nu, Ex_bar, ...
+                    sigma);
                 % Maximizes the TEQ MFB that considers the minimzed MSE in
                 % the denominator:
                 objective(i,k) = SNRteq;
             case 1 % SSNR
-                criterion_label = 'SSNR';
+                type_label = 'SSNR';
+                objective_label = '10\\log(SSNR)';
+                % Design the TEQ and evaluate the resulting SSNR:
+                [w, SSNR] = ssnr_teq(p, l, delta, floor(nTaps/l), nu);
                 % Maximizes the Shortening SNR
-                objective(i,k) = teqSSNR( w, p, delta, nu );
+                objective(i,k) = SSNR;
             case 2 % Geo SNR
                 % Maximizes the Geometric (multich-channel) SNR
-                criterion_label = 'GeoSNR';
+                type_label = 'GeoSNR';
                 % New effective channel (see Eq. 7 in [2]):
                 B_i = fft(b, N);
                 W_i = fft(w, N);
@@ -83,27 +154,35 @@ for i = 1:nTap_length
 
                 objective(i,k) = L_b;
             otherwise
-                error('Non-available TEQ optimization criterion');
+                error('Non-available TEQ type');
         end
     end
 end
 
-%% Results
-
-% 3-D plot of the SNRs
-figure
-surf(delta_vec, L_vec, objective)
-xlabel('Delay (Delta)')
-ylabel('TEQ Length (L)')
-zlabel('Objective')
-title('TEQ Delay and Length Search')
-title(sprintf('Optimized in terms of %s', criterion_label));
+%% Delay and equalizer length choices
 
 % Find the optimal combination of number of taps and delay:
 maxSnr = max(objective(~isnan(objective)));
 [i, k] = find(objective == maxSnr);
-nTaps_o = min(L_vec(i));
+nTaps_o = min(nTap_vec(i));
 delta_o = min(delta_vec(k));
+
+% TODO: consider the optimal tradeoff curve of the multiobjective
+% optimization and avoid obtaining a significantly longer length at the
+% expense of minor gain in the objective.
+
+%% Results
+
+if (show_plots)
+    % 3-D plot of the cost function
+    figure
+    surf(delta_vec, nTap_vec, 10*log10(objective))
+    xlabel('Delay (Delta)', 'Interpreter', 'latex')
+    ylabel('TEQ Length (L)', 'Interpreter', 'latex')
+    zlabel(objective_label, 'Interpreter', 'latex')
+    title('TEQ Delay and Length Search', 'Interpreter', 'latex')
+    title(sprintf('TEQ optimized in terms of %s', type_label));
+end
 
 end
 
