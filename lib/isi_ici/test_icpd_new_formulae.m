c3411dd342fd1ce38ba76e836de9946dd69fb1e0
Igor Freire <igor.auad@gmail.com>
2016-08-02 16:45:04 -0300

    Consider also when windowing is not used in ICPD formulae test script

--- lib/isi_ici/test_icpd_new_formulae.m
+++ lib/isi_ici/test_icpd_new_formulae.m
@@ -1,48 +1,67 @@
-clear all
-clc
+clearvars, clc
+addpath(genpath('../'))
 
 %% Parameters
-Nfft = 4;
-Lcp = 2;
-Lcs = 2;
-n0 = 1;
-nSymbols = 2;
-
-h = [0 1 0.9 0.3].';
+Nfft      = 4;
+nu        = 2;
+tau       = 2;
+n0        = 1;
+nSymbols  = 2;
+windowing = 0;
+% CIR:
+h = [0 0.1 1 0.9 0.3 0.2 0.1].';
 
 %% Derived parameters
-symbolSize = Nfft + Lcp;
-Lh = length(h);
-L = Lh - 1;
-windowStart = (n0 + 1);
-windowEnd   = nSymbols*symbolSize + windowStart - 1;
+symbolSize  = Nfft + nu;
+Lh          = length(h);
+L           = Lh - 1;
 
-% Window:
-dmtWindow = designDmtWindow(Nfft, Lcp, Lcs);
+% Start/end index boundaries after symbol timing recovery at the receiver:
+rxStart = (n0 + 1);
+rxEnd   = nSymbols*symbolSize + rxStart - 1;
 
-nAffected = L - (Lcp - Lcs) - n0; % Number of affected samples
+% Number of samples affected by post-cursor ICPD:
+if (windowing)
+    delta = L - (nu - tau) - n0
+else
+    delta = L - nu - n0
+    % Force "tau" (CS length) to 0 when windowing is not used
+    tau = 0;
+end
+
+% Window applied to CP and CS:
+dmtWindow = designDmtWindow(Nfft, nu, tau);
+
+if (delta <= 0)
+    warning('Parameters yield absent post-cursor ICPD already');
+end
+%% Circulant channel matrix
+
+h_padded = [h;  zeros(Nfft - Lh, 1)];
+Hcirc = toeplitz(h_padded, ...
+    flipud(circshift(h_padded,-1)));
 
 %% Transmission
 
 x = rand(Nfft, nSymbols + 1);
 
-x_ce = [x(end -Lcp + 1:end,:); x; x(1:Lcs,:)];
+if (windowing)
+    x_ce = [x(end -nu + 1:end,:); x; x(1:tau,:)];
 
-x_ce = windowAndOverlap(x_ce, dmtWindow, Nfft, Lcp, Lcs);
+    x_ce = windowAndOverlap(x_ce, dmtWindow, Nfft, nu, tau);
+else
+    x_ce = [x(end -nu + 1:end,:); x];
+end
 
 x_stream = x_ce(:);
 
 y_stream = conv(x_stream, h);
 
-y_stream = y_stream(windowStart:windowEnd);
+y_stream = y_stream(rxStart:rxEnd);
 
 y_ce = reshape(y_stream, symbolSize, nSymbols);
 
-y = y_ce((Lcp + 1):(Lcp + Nfft), :);
-
-h_padded = [h;  zeros(Nfft - Lh, 1)];
-Hcirc = toeplitz(h_padded, ...
-    flipud(circshift(h_padded,-1)));
+y = y_ce((nu + 1):(nu + Nfft), :);
 
 %% Test Formulas
 
@@ -59,24 +78,28 @@ Hisi = zeros(Nfft,Nfft);
 Hici = zeros(Nfft,Nfft);
 
 % Window
-W_t = diag(dmtWindow(end-nAffected+1:end));
+W_t = diag(dmtWindow(end-delta+1:end));
 
-Ht = toeplitz([h(end) zeros(1, L-(Lcp-Lcs)-n0-1)], flipud(h((Lcp - Lcs + n0 + 2):end)));
-
+% ISI Core Matrix
+Ht = toeplitz([h(end) zeros(1, L-(nu-tau)-n0-1)], flipud(h((nu - tau + n0 + 2):end)));
 Ht_windowed = Ht * W_t;
+% IMPORTANT: the above works both when windowing is used and when it is not
+% used. The only condition is that tau must be set to 0 when not using
+% windowing, first because it is really 0, second because W_t becomes an
+% identity.
 
 % Post-cursor ISI
-if (n0 >= Lcs)
-    Hisi(1:(nAffected), (Nfft - L + Lcp + 1):(Nfft + Lcs - n0) ) = Ht_windowed;
+if (n0 >= tau)
+    Hisi(1:(delta), (Nfft - L + nu + 1):(Nfft + tau - n0) ) = Ht_windowed;
 else
-    shiftBy = Lcs - n0;
-    Hisi(1:(nAffected), (Nfft - L + Lcp + 1):(Nfft + Lcs - n0) ) = Ht_windowed;
+    shiftBy = tau - n0;
+    Hisi(1:(delta), (Nfft - L + nu + 1):(Nfft + tau - n0) ) = Ht_windowed;
     Hisi = Hisi(:,shiftBy+1:end);
     Hisi = circshift(Hisi, [0, shiftBy]);
 end
 
 % Post-cursor ICI
-Hici(1:(nAffected), (Nfft - L + 1):(Nfft -Lcp + Lcs - n0)) = -Ht_windowed;
+Hici(1:(delta), (Nfft - L + 1):(Nfft -nu + tau - n0)) = -Ht_windowed;
 
 y_postIci = Hici * circshift(x, -n0);
 y_postIsi = Hisi * circshift(x(:,1:end-1), -n0);
@@ -104,7 +127,7 @@ y_preIci = HpreIci * circshift(x, -n0);
 
 % Pre-cursor ISI
 HpreIsi = zeros(Nfft, Nfft);
-HpreIsi(end-n0+1:end,(Nfft - Lcp - n0 + 1):(Nfft - Lcp)) = H_tilde_t_windowed;
+HpreIsi(end-n0+1:end,(Nfft - nu - n0 + 1):(Nfft - nu)) = H_tilde_t_windowed;
 
 % Multiply by "future" symbols:
 y_preIsi = HpreIsi * circshift(x_withLastSymbol(:, 2:end), -n0);
@@ -112,5 +135,6 @@ y_preIsi = HpreIsi * circshift(x_withLastSymbol(:, 2:end), -n0);
 disp('Test removing pre-cursor ICPD:')
 y - (y_ideal + y_preIci + y_preIsi)
 
+%% Complete ICPD Removal
 disp('Test removing post and pre-cursor ICPD:')
 y - (y_ideal + y_postIci + y_postIsi + y_preIci + y_preIsi)
